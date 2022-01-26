418d61c57cee5073ba0b03aec38ed6e163eace0a
Deva Sagar <deva.sagar@oracle.com>
2022-01-26 16:09:15 -0500

    Add JenkinsfileDB and refactor setup tunnel script to handle bastion in given compartment and use given port

--- ci/scripts/lre_setup_ssh_tunnel.sh
+++ ci/scripts/lre_setup_ssh_tunnel.sh
@@ -12,18 +12,21 @@ if [ -z "${ssh_public_key_path}" ] ; then
     echo "ssh_public_key_path env var must be set!"
     exit 1
 fi
-if [ -z "${dev_lre_compartment_id}" ] ; then
-    echo "dev_lre_compartment_id env var must be set!"
+if [ -z "${bastion_compartment_id}" ] ; then
+    echo "bastion_compartment_id env var must be set!"
     exit 1
 fi
-if [ -z "${KUBECONFIG}" ] ; then
-    echo "KUBECONFIG env var must be set!"
+if [ -z "${bastion_session_port}" ] ; then
+    echo "bastion_session_port env var must be set!"
     exit 1
 fi
-
+if [ -z "${bastion_session_local_port}" ] ; then
+    echo "bastion_session_local_port env var is not set, using ${bastion_session_port}"
+    bastion_session_local_port=$bastion_session_port
+fi
 
 BASTION_ID=$(oci bastion bastion list \
-            --compartment-id "${dev_lre_compartment_id}" --all \
+            --compartment-id "${bastion_compartment_id}" --all \
             | jq -r '.data[0]."id"')
 
 if [ -z "$BASTION_ID" ]; then
@@ -35,7 +38,7 @@ SESSION_ID=$(oci bastion session create-port-forwarding \
    --bastion-id $BASTION_ID \
    --ssh-public-key-file ${ssh_public_key_path} \
    --target-private-ip 10.196.0.171 \
-   --target-port 6443 | jq '.data.id' | sed s/\"//g)
+   --target-port $bastion_session_port | jq '.data.id' | sed s/\"//g)
 
 if [ -z "$SESSION_ID" ]; then
     echo "Failed to create a bastion session"
@@ -49,7 +52,7 @@ COMMAND=`oci bastion session get  --session-id=${SESSION_ID} | \
   jq '.data."ssh-metadata".command' | \
   sed 's/"//g' | \
   sed 's|<privateKey>|'"${ssh_private_key_path}"'|g' | \
-  sed 's|<localPort>|6443|g'`
+  sed "s|<localPort>|${bastion_session_local_port}|g"`
 echo "command = ${COMMAND}"
 if [ -z "$COMMAND" ]; then
     echo "didn't find the command to set up ssh tunnel"
